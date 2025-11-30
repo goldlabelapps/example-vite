eda8a8f7d7efa09a81dad8ac6b3fb2d898f80ffa
Goldlabel Apps Ltd <github@goldlabel.pro>
2025-11-30 05:50:08 +0000

    import package

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import {Bootstrap} from '@goldlabelapps/core';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() {  
   return (
     <>
-      <div>
+      @goldlabelapps/core
+    </>
+  )
+}
+
+export default App
+
+/*
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const [count, setCount] = useState(0)
+
+<div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +36,4 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
-
-export default App
+*/
